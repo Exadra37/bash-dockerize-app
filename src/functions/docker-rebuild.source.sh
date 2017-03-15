291687b71993bc72cb698b885b0e915820c00a0d
Exadra37 <exadra37@gmail.com>
2017-03-15 21:46:18 +0000

    Update doc blocks.

--- src/functions/docker-rebuild.source.sh
+++ src/functions/docker-rebuild.source.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # @package exadra37-bash/dockerize-app
 # @link    https://gitlab.com/u/exadra37-bash/dockerize-app
-# @since   2017/03/10
+# @since   10 March 2017
 # @license GPL-3.0
 # @author  Exadra37(Paulo Silva) <exadra37ingmailpointcom>
 #
 # Social Links:
 # @link    Auhthor:  https://exadra37.com
-# @link    Github:   https://gitlab.com/Exadra37
+# @link    Gitlab:   https://gitlab.com/Exadra37
 # @link    Github:   https://github.com/Exadra37
 # @link    Linkedin: https://uk.linkedin.com/in/exadra37
 # @link    Twitter:  https://twitter.com/Exadra37
